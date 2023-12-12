d03f2313018e911fae4bb1c63c35291b85e7e5aa
Petra Budi Karunia <petrabudikarunia@Petras-MacBook-Pro.local>
2023-12-12 17:07:08 +0700

    Add main structure

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Homepage
-    </div>
+    <>
+      <Header/>
+      <Main/>
+      <Footer/>
+    </>
   );
 }
 
---
+++ src/Header.js
@@ -0,0 +1,9 @@
+
+function Header(){
+    return(
+        <>
+            <img src='' />
+            <Nav/>
+        </>
+    )
+}
